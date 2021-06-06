99c1776292fc9eb7a0de1c38ff02119f77cfd36f
satwik-talluri <satwiktalluri1@gmail.com>
2021-06-06 22:55:41 +0530

    Created database model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TodoItem(models.Model):
+	auto_inc_id = models.AutoField(primary_key=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	item = models.TextField()
+	date_created = models.DateTimeField(auto_now_add=True)
