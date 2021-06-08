2fcc2bf14607f404f1f51981f0eae5051fa90c94
satwik-talluri <satwiktalluri1@gmail.com>
2021-06-08 13:35:51 +0530

    Created Update and delete options

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import TodoItem
 
-def home(request):
-	context = {
-		'todoitem': TodoItem.objects.all(),
-	}
-	return render(request, 'todo/home.html', context)
+
+class TodoListView(ListView):
+	model = TodoItem
+	template_name = 'todo/home.html'
+	context_object_name = 'todoitem'
+	ordering = ['date_created']
+
+class TodoCreateView(CreateView):
+	model = TodoItem
+	fields = ['author', 'item']
+
+class TodoUpdateView(UpdateView):
+	model = TodoItem
+	fields = ['author', 'item']
+
+class TodoDeleteView(DeleteView):
+	model = TodoItem
+	success_url = '/'
+
